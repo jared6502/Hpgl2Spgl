55943857d09e26f81d7d8376dedb3d8bce945d78
jared6502 <jared6502@gmail.com>
2017-01-15 17:05:43 -0500

    initial commit of code

---
+++ HPGL_2_SPGL/frmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace HPGL_2_SPGL
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInputFile = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtOutputFile = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInputFile
+            // 
+            this.txtInputFile.Location = new System.Drawing.Point(12, 12);
+            this.txtInputFile.Name = "txtInputFile";
+            this.txtInputFile.Size = new System.Drawing.Size(336, 20);
+            this.txtInputFile.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(354, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(36, 20);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtOutputFile
+            // 
+            this.txtOutputFile.Location = new System.Drawing.Point(12, 38);
+            this.txtOutputFile.Name = "txtOutputFile";
+            this.txtOutputFile.Size = new System.Drawing.Size(336, 20);
+            this.txtOutputFile.TabIndex = 2;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(113, 64);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "Convert";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(207, 64);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Exit";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 95);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtOutputFile);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtInputFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.Text = "HPGL to SPGL Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputFile;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtOutputFile;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ HPGL_2_SPGL/frmMain.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HPGL_2_SPGL
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.FileName = txtInputFile.Text;
+
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                txtInputFile.Text = ofd.FileName;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(txtOutputFile.Text))
+            {
+                DialogResult result = MessageBox.Show("Replace existing file?", "File Already Exists", MessageBoxButtons.OKCancel);
+                if (result != System.Windows.Forms.DialogResult.OK)
+                {
+                    return;
+                }
+            }
+
+            Convert();
+        }
+
+        void Convert()
+        {
+            try
+            {
+                using (System.IO.StreamReader infile = new System.IO.StreamReader(new System.IO.FileStream(txtInputFile.Text, System.IO.FileMode.Open)))
+                using (System.IO.StreamWriter outfile = new System.IO.StreamWriter(new System.IO.FileStream(txtOutputFile.Text, System.IO.FileMode.Create)))
+                {
+                    string input;
+                    string output;
+                    bool absolute = true;
+                    bool up = true;
+
+                    while (!infile.EndOfStream)
+                    {
+                        input = infile.ReadLine();
+
+                        switch(input.Substring(0, 2))
+                        {
+                            case "IN":
+                                output = "IN;";
+                                outfile.WriteLine(output);
+                                break;
+
+                            case "PU":
+                                if (input == "PU;")
+                                {
+                                    output = input;
+                                    outfile.WriteLine(output);
+                                }
+                                else
+                                {
+                                    output = "PU;";
+                                    outfile.WriteLine(output);
+                                    output = (absolute ? "MA " : "MR ") + input.Substring(2, input.Length - 2);
+                                    outfile.WriteLine(output);
+                                }
+
+                                up = true;
+                                break;
+
+                            case "PD":
+                                if (input == "PD;")
+                                {
+                                    output = input;
+                                    outfile.WriteLine(output);
+                                }
+                                else
+                                {
+                                    output = "PD;";
+                                    outfile.WriteLine(output);
+                                    output = (absolute ? "DA " : "DR ") + input.Substring(2, input.Length - 2);
+                                    outfile.WriteLine(output);
+                                }
+
+                                up = false;
+                                break;
+
+                            case "PA":
+                                absolute = true;
+
+                                if (input == "PA;")
+                                {
+                                    //swallow this command
+                                }
+                                else
+                                {
+                                    output = (up ? "M" : "D") + (absolute ? "A " : "R ") + input.Substring(2, input.Length - 2);
+                                    outfile.WriteLine(output);
+                                }
+                                break;
+
+                            case "PR":
+                                absolute = false;
+
+                                if (input == "PR;")
+                                {
+                                    //swallow this command
+                                }
+                                else
+                                {
+                                    output = (up ? "M" : "D") + (absolute ? "A " : "R ") + input.Substring(2, input.Length - 2);
+                                    outfile.WriteLine(output);
+                                }
+                                break;
+
+                            case "AA":
+                                //TODO
+                                break;
+
+                            case "AR":
+                                //TODO
+                                break;
+
+                            case "VS":
+                                output = input;
+                                outfile.WriteLine(output);
+                                break;
+
+                            default:
+                                //command dropped
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception thrown:\n" + ex.Message, "Error during conversion");
+            }
+        }
+    }
+}
